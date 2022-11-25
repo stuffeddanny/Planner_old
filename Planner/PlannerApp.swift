1770b5d9d2cb6b650eac0fb347239abcb4457f06
Danylo Siefierov <93935347+stuffeddanny@users.noreply.github.com>
2022-11-25 16:25:48 +0000

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Danny on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
