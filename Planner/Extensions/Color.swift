fc64016581adadb045f673a4678c42f9caf5d123
Danylo Siefierov <93935347+stuffeddanny@users.noreply.github.com>
2022-11-26 16:15:15 +0000

    Color extension

---
+++ Planner/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  Planner
+//
+//  Created by Danny on 11/26/22.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let launchBackground = Color("launchBackground")
+    let primaryOpposite = Color("primaryOpposite")
+    
+}
+
